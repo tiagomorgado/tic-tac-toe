9bd19d3fa303327092a1dba7fa5c1c86eacd79a9
Tiago Morgado <Tiago.alcm@icloud.com>
2023-05-26 21:11:07 +0200

    added restarrt button

--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -1,10 +1,12 @@
 import Board from "./Board";
+import Restart from "./Restart";
 
 const Game = () => {
     return (
       <div className="game">
         Tic-Tac-Toe
         <Board />
+        <Restart/>
       </div>
     );
   };
---
+++ src/components/Restart.jsx
@@ -0,0 +1,8 @@
+const Restart = () => {
+  return (
+    <>
+    <button className='restartBtn' onClick={() => window.location.reload()}>Restart Game</button>
+    </>
+  )
+}
+export default Restart
